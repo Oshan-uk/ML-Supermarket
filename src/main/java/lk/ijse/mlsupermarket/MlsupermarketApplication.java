737de72670586dcbd4488196f7e579f077100a03
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-10 19:06:00 +0530

    Creat project

---
+++ src/main/java/lk/ijse/mlsupermarket/MlsupermarketApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.mlsupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlsupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlsupermarketApplication.class, args);
+	}
+
+}
