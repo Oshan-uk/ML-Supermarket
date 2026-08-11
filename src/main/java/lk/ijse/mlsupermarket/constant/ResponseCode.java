a7afdb17cafbaeb62a6ef6183dd5d6967d3d0d3c
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-11 08:36:49 +0530

    Create constant package

---
+++ src/main/java/lk/ijse/mlsupermarket/constant/ResponseCode.java
@@ -0,0 +1,6 @@
+package lk.ijse.mlsupermarket.constant;
+
+public class ResponseCode {
+    public static int OPERATION_SUCCESS = 0;
+    public static int OPERATION_FAILURE = 1;
+}
---
+++ src/main/java/lk/ijse/mlsupermarket/constant/ResponseMessage.java
@@ -0,0 +1,6 @@
+package lk.ijse.mlsupermarket.constant;
+
+public class ResponseMessage {
+    public static String SUCCESS_MESSAGE = "SUCCESS";
+    public static String TOKEN = "JWT_TOKEN";
+}
