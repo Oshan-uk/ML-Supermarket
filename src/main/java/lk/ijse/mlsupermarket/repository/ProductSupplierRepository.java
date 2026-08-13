26aadd0d2cd3e3f8f6f4c7ad6d95646593bc0e03
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-13 19:55:43 +0530

    Updated ProductSupplierRepository

--- src/main/java/lk/ijse/mlsupermarket/repository/ProductSupplierRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/ProductSupplierRepository.java
@@ -1,9 +1,14 @@
 package lk.ijse.mlsupermarket.repository;
 
 import lk.ijse.mlsupermarket.entity.Product;
+import lk.ijse.mlsupermarket.entity.ProductSupplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductSupplierRepository extends JpaRepository<Product, Long> {
+public interface ProductSupplierRepository extends JpaRepository<ProductSupplier, Long> {
+
+    List<ProductSupplier> findBySupplier_SupplierId(Long supplierId);
 }
