3c36a27bcac177a24e7f32f1521655ee6a0ade4b
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-13 19:44:42 +0530

    Updated CategoryRepository

--- src/main/java/lk/ijse/mlsupermarket/repository/CategoryRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/CategoryRepository.java
@@ -1,9 +1,16 @@
 package lk.ijse.mlsupermarket.repository;
 
+import lk.ijse.mlsupermarket.dto.CategoryDTO;
 import lk.ijse.mlsupermarket.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query(value = "Select new lk.ijse.mlsupermarket.dto.CategoryDTO (c.categoryId, c.categoryName, c.description) from Category c where (?1 Is null or c.categoryName like %?1%)")
+    List<CategoryDTO> searchCategoriesByName(String categoryName);
 }
