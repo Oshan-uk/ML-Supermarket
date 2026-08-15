b9cc8bf2eef5d9638b89d6199c3ca68d838a9269
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-15 19:58:19 +0530

    Created UserServiceImpl with implementation

--- src/main/java/lk/ijse/mlsupermarket/service/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/mlsupermarket/service/impl/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package lk.ijse.mlsupermarket.service.impl;
 
+import jakarta.validation.Valid;
 import lk.ijse.mlsupermarket.dto.UserDTO;
+import lk.ijse.mlsupermarket.entity.User;
 import lk.ijse.mlsupermarket.repository.UserRepository;
 import lk.ijse.mlsupermarket.service.UserService;
+import lk.ijse.mlsupermarket.status.UserStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -23,26 +29,110 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(UserDTO userDTO) {
+        log.info("Execute saveUser() dto {}", userDTO);
+        try {
+            User user = new User();
+            user.setUserName(userDTO.getUserName());
+            user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+            user.setContact(userDTO.getContact());
+            user.setUserRole(userDTO.getUserRole());
+            user.setStatus(UserStatus.ACTIVE);
 
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.error("Error in saveUser()");
+            throw e;
+        }
     }
+
 
     @Override
     public void updateUser(UserDTO userDTO) {
+        log.info("Execute updateUser()");
+        try{
+            Optional<User> optionalUser = userRepository.findById(userDTO.getUserId());
 
+            if(optionalUser.isEmpty())throw new RuntimeException("Sorry, related User NOT FOUND!");
+
+            User user = optionalUser.get();
+            user.setUserName(userDTO.getUserName());
+            user.setContact(userDTO.getContact());
+            user.setUserRole(userDTO.getUserRole());
+
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.error("Error in updateUser()");
+            throw e;
+        }
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return List.of();
+        log.info("Execute getAllUsers()");
+
+        List<User> users = userRepository.findAll();
+        List<UserDTO> userDTOList = new ArrayList<>();
+
+        for (User user : users) {
+            UserDTO userDTO = new UserDTO(
+                    user.getUserId(),
+                    user.getUserName(),
+                    null,
+                    user.getContact(),
+                    user.getUserRole(),
+                    user.getStatus()
+            );
+
+            userDTOList.add(userDTO);
+        }
+
+        return userDTOList;
     }
 
     @Override
     public UserDTO getUserById(long userId) {
-        return null;
+        log.info("Executes getUserById()");
+
+        try{
+            Optional optionalUser = userRepository.findById(userId);
+            if (optionalUser.isEmpty()) throw new RuntimeException("Sorry, Related User NOT Found!");
+
+            User user = new User();
+            return new UserDTO(
+                    user.getUserId(),
+                    user.getUserName(),
+                    null,
+                    user.getContact(),
+                    user.getUserRole(),
+                    user.getStatus()
+            );
+
+        }catch(Exception e){
+            log.error("Error in getUserNyId()");
+            throw e;
+
+        }
     }
 
     @Override
     public void updateUserStatus(long userId) {
+        log.info("Execute updateUserStatus()");
+
+        try{
+            Optional<User> optionalUser = userRepository.findById(userId);
+            if(optionalUser.isEmpty()) throw new RuntimeException("Sorry, related user in not found!");
+
+            User user = optionalUser.get();
+            user.setStatus(
+                    user.getStatus() == UserStatus.ACTIVE ? UserStatus.INACTIVE : UserStatus.ACTIVE
+            );
+
+            userRepository.save(user);
+
+        }catch(Exception e){
+            log.error("Error in updateUserStatus()");
+            throw e;
+        }
 
     }
 }
