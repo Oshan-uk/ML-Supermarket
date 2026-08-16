ad7c656903f3033703e7e4e0143d84176623936c
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-16 10:07:52 +0530

    Updated InventoryRepository

--- src/main/java/lk/ijse/mlsupermarket/repository/InventoryRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/InventoryRepository.java
@@ -4,6 +4,12 @@ import lk.ijse.mlsupermarket.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProduct_ProductId(Long productId);
+    List<Inventory>findByQuantityLessThanEqual(Integer reorderLevel);
+
 }
