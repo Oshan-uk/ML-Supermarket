0a658f818483e7606ea7c2a2c81cc55bf69695b4
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-16 10:19:44 +0530

    Updated CustomerRepository with searchCustomersByName()

--- src/main/java/lk/ijse/mlsupermarket/repository/CustomerRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/CustomerRepository.java
@@ -1,9 +1,16 @@
 package lk.ijse.mlsupermarket.repository;
 
+import lk.ijse.mlsupermarket.dto.CustomerDTO;
 import lk.ijse.mlsupermarket.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query(value = "SELECT new lk.ijse.mlsupermarket.dto.CustomerDTO(c.customerId, c.customerName, c.contact, c.email) FROM Customer c WHERE (?1 IS NULL OR c.customerName LIKE %?1%)")
+    List<CustomerDTO> searchCustomersByName(String customerName);
 }
