3031d05adf8bbec0b26cec2914c7d037df9c064b
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-16 10:10:04 +0530

    Updated StockMovementRepository

--- src/main/java/lk/ijse/mlsupermarket/repository/StockMovementRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/StockMovementRepository.java
@@ -4,6 +4,9 @@ import lk.ijse.mlsupermarket.entity.StockMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StockMovementRepository extends JpaRepository<StockMovement, Long> {
+    List<StockMovement> findByProduct_productId(Long productId);
 }
