31ca7c0ab5bd49522b2c287316e5067456dcb4df
Oshan-uk <oshanudeesha2004@gmail.com>
2026-08-19 20:52:40 +0530

    Updated PaymentRepository

--- src/main/java/lk/ijse/mlsupermarket/repository/PaymentRepository.java
+++ src/main/java/lk/ijse/mlsupermarket/repository/PaymentRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findByOrder_OrderID(Long orderId);
+    Optional<Payment> findByOrder_OrderId(Long orderId);
 }
